1a546de45d90f1732fcb55e970647387d10041e2
Changhun Kim <ssohot1@kaist.ac.kr>
2020-01-20 14:40:51 +0900

    Tab1

---
+++ tab2.py
@@ -0,0 +1,19 @@
+import main
+import tab1
+
+import sys
+import time
+import copy
+
+import cv2
+from PyQt5.QtCore import *
+from PyQt5 import QtGui
+# from PyQt5 import QtM
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from tensorflow.keras.models import load_model
+
+class SecondTab(QWidget):
+    def __init(self):
+        print("Hi")
+        super().__init__()
